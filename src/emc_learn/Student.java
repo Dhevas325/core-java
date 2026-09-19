bbf0814a163da4b7cd20528b7a6b5cfafab4a9a4
Dhevas325 <dhevasundhari03@gmail.com>
2026-09-19 12:35:19 +0530

    Add emc_learn Eclipse Java project

---
+++ src/emc_learn/Student.java
@@ -0,0 +1,14 @@
+package emc_learn;
+
+public class Student {
+	Student(){// constructor
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ob1=new Student();
+
+	}
+
+}
